137511470dbdaf4b7bb05f16e9d913cbe80a64a7
Muqsit <notmuqsit+a4i@gmail.com>
2026-07-11 16:47:33 +0800

    frontend: assume postbuild .js assets are immutable

--- shiksha-website/shiksha-frontend/postbuild.js
+++ shiksha-website/shiksha-frontend/postbuild.js
@@ -6,3 +6,16 @@ const healthPath = `${outputPath}/health.json`;
 const health = JSON.parse(fs.readFileSync(healthPath));
 health.build = process.env.SHIKSHA_COPILOT_BUILD || null;
 fs.writeFileSync(healthPath, JSON.stringify(health, null, 2));
+
+fs.writeFileSync(`${outputPath}/staticwebapp.config.json`, JSON.stringify({
+    routes: fs.readFileSync(`${outputPath}/_headers`, "utf8").trim().split(/\n\s*\n/).map(block => {
+        const [route, ...headers] = block.split("\n");
+        return {
+            route,
+            headers: Object.fromEntries(headers.map(header => {
+                const [name, ...value] = header.trim().split(":");
+                return [name, value.join(":").trim()];
+            }))
+        };
+    })
+}, null, 2));
